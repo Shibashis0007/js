6d1f6d7e0fcf9df0d4f4de121eca6d14ff4b2006
Shibashis0007 <164138144+Shibashis0007@users.noreply.github.com>
2024-04-15 06:17:47 +0000

    variable covered

---
+++ basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 276359
+let accountEmail = "klm@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jap"
+let accountState;
+
+// accountId = 4 // not allowed
+
+// accountEmail = "qwe@gmail.com"
+// accountPassword = "97659"
+// accountCity = "USA"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
